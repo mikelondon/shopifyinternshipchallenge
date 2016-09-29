eaa5c43841183e6469463ec7e3e6e8b8503129fc
Mike London <mikelawndawn@gmail.com>
2016-09-28 18:09:32 -0700

    Sum the price of one page

--- challengeJSON.rb
+++ challengeJSON.rb
@@ -1,16 +1,52 @@
 require "json"
 require 'net/http'
+require 'pry'
 
-
-urls = ['http://shopicruit.myshopify.com/products.json?page=1','http://shopicruit.myshopify.com/products.json?page=2','http://shopicruit.myshopify.com/products.json?page=3','http://shopicruit.myshopify.com/products.json?page=4','http://shopicruit.myshopify.com/products.json?page=5']
-
+store_page_urls = ['http://shopicruit.myshopify.com/products.json?page=1']
 total_json_database = []
 
 
-urls.each do |url|
+store_page_urls.each do |url|
     uri = URI(url)
     response = Net::HTTP.get(uri)
-    total_json_database << JSON.parse(response) # turn the JSON response into a Ruby Hash
+    total_json_database << JSON.parse(response)
 end
 
-puts total_json_database
+
+single_page_price = 0
+
+
+total_json_database.each do |page|
+    page['products'].each do |item|
+        item_price = item['variants'][0]['price'].to_f
+        single_page_price += item_price
+    end
+    puts single_page_price
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ,'http://shopicruit.myshopify.com/products.json?page=2','http://shopicruit.myshopify.com/products.json?page=3','http://shopicruit.myshopify.com/products.json?page=4','http://shopicruit.myshopify.com/products.json?page=5'
